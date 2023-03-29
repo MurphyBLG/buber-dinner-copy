3f37ddf5a0a38d74fa68c2ed133f4ce9b4ed43a8
Vitaliy Ryavkin <vitaliy.ryavkin@gmail.com>
2023-03-29 21:48:27 +0500

    First Lesson Finished

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using BuberDinner.Contracts.Authentication;
+﻿using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
@@ -7,15 +8,32 @@ namespace BuberDinner.Api.Controllers;
 [Route("/api/[controller]")]
 public class AuthenticationController : Controller
 {
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
     [HttpPost("register")]
     public IActionResult Register(RegisterRequest registerRequest)
     {
-        return Ok(registerRequest);
+        var authResult = _authenticationService.Register(
+            registerRequest.FirstName,
+            registerRequest.Lastname,
+            registerRequest.Email,
+            registerRequest.Password);
+
+        return Ok(authResult);
     }
 
     [HttpPost("login")]
     public IActionResult Login(LoginRequest loginRequest)
     {
-        return Ok(loginRequest);
+        var authResult = _authenticationService.Login(
+            loginRequest.Email,
+            loginRequest.Password);
+
+        return Ok(authResult);
     }
 }

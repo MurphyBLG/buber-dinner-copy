89fdbd7aad20426145c969117505dd1a5c69033f
Vitaliy Ryavkin <vitaliy.ryavkin@gmail.com>
2023-07-22 23:18:26 +0500

    Correct type of Problem class used for validation error.

--- BuberDinner.Api/Controllers/ApiController.cs
+++ BuberDinner.Api/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 using BuberDinner.Api.Common.Http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.Api.Controllers;
 
@@ -9,6 +10,14 @@ public class ApiController : Controller
 {
     protected IActionResult Problem(List<Error> errors)
     {
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+            errors.ForEach(error => modelStateDictionary.AddModelError(error.Code, error.Description));
+            return ValidationProblem(modelStateDictionary);
+        }
+            
+
         HttpContext.Items[HttpContextItemKeys.Errors] = errors;
         var firstError = errors[0];
         var statusCode = firstError.Type switch

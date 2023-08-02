dd7736592aa7286ac9e9dde51422da22a075c599
Vitaliy Ryavkin <vitaliy.ryavkin@gmail.com>
2023-08-02 20:22:29 +0500

    Domain creation started

---
+++ BuberDinner.Domain/Menu/Menu.cs
@@ -0,0 +1,55 @@
+﻿using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Dinner.ValueObjects;
+using BuberDinner.Domain.Host.ValueObjects;
+using BuberDinner.Domain.Menu.Entities;
+using BuberDinner.Domain.Menu.ValueObjects;
+using BuberDinner.Domain.MenuReview.ValueObjects;
+
+namespace BuberDinner.Domain.Menu;
+
+public sealed class Menu : AggregateRoot<MenuId>
+{
+    private readonly List<MenuSection> _sections = new();
+    private readonly List<DinnerId> _dinnerIds = new();
+    private readonly List<MenuReviewId> _menuReviewIds = new();
+    public string Name { get; }
+    public string Description { get; }
+    public float AverageRating { get; }
+    public HostId HostId { get; }
+    public IReadOnlyList<MenuSection> Sections => _sections.AsReadOnly();
+    public IReadOnlyList<DinnerId> DinnerIds => _dinnerIds.AsReadOnly();
+    public IReadOnlyList<MenuReviewId> MenuReviewIds => _menuReviewIds.AsReadOnly();
+   
+    public DateTime CreateDateTime { get; }
+    public DateTime UpdateDateTime { get; }
+
+    private Menu(
+        MenuId id,
+        string name,
+        string description,
+        HostId hostId,
+        DateTime createDateTime,
+        DateTime updateDateTime) 
+        : base(id)
+    {
+        Name = name;
+        Description = description;
+        HostId = hostId;
+        CreateDateTime = createDateTime;
+        UpdateDateTime = updateDateTime;
+    }
+
+    public static Menu CreateUnique(
+        string name,
+        string description,
+        HostId hostId)
+    {
+        return new(
+            MenuId.CreateUnique(),
+            name,
+            description,
+            hostId,
+            DateTime.UtcNow,
+            DateTime.UtcNow);
+    }
+}

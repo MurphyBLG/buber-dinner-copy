eb123159c982aa80787dbdfc96bb72e3b8361eba
Vitaliy Ryavkin <vitaliy.ryavkin@gmail.com>
2023-08-04 16:30:01 +0500

    User aggregate template done

---
+++ BuberDinner.Domain/User/User.cs
@@ -0,0 +1,47 @@
+﻿using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.User.ValueObjects;
+
+namespace BuberDinner.Domain.User;
+
+public class User : AggregateRoot<UserId>
+{
+    public string FirstName { get; } = null!;
+    public string LastName { get; } = null!;
+    public string Email { get; } = null!;
+    public string Password { get; } = null!; // Shouldn't store like that
+    public DateTime CreateDateTime { get; } 
+    public DateTime UpdateDateTime { get; }
+
+    private User(
+        UserId id,
+        string firstName,
+        string lastName,
+        string email,
+        string password,
+        DateTime createdDateTime,
+        DateTime updatedDateTime) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+        CreateDateTime = createdDateTime;
+        UpdateDateTime = updatedDateTime;
+    }
+
+    public static User Create(
+        string firstName,
+        string lastName,
+        string email,
+        string password)
+    {
+        return new(
+            UserId.CreateUnique(),
+            firstName,
+            lastName,
+            email,
+            password,
+            DateTime.UtcNow,
+            DateTime.UtcNow);   
+    }
+}
